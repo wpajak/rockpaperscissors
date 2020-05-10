ab5692d66804d10eec6fdbc575418407116d70b0
Wojciech Pajak <wojciechpaj@gmail.com>
2020-05-10 12:04:46 +0200

    Add comments

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,14 +1,14 @@
 from random import choice
 options_comp = ("ROCK","PAPER","SCISSORS")
 options_player = ("ROCK","PAPER","SCISSORS","QUIT")
-while True:
-    player = input("\nPlease insert your choice: ROCK, PAPER or SCISSORS. Input QUIT to terminate.\n\n").upper()
-    while player not in options_player:
+while True: ##repeats the game until user wishes to quit
+    player = input("\nPlease insert your choice: ROCK, PAPER or SCISSORS. Input QUIT to terminate.\n\n").upper() ##gets user choice
+    while player not in options_player: ##checks if user choice is valid
         player = input("\nPlease insert a valid choice: ROCK, PAPER or SCISSORS. Input QUIT to terminate.\n\n").upper()
-    if player =="QUIT":
+    if player =="QUIT": ##terminates the game when user inputs QUIT
         break
 
-    computer = choice(options_comp)
+    computer = choice(options_comp) ##generates random computer choice
     print (f"\nComputer plays {computer}\n")
 
     if player == "ROCK":
