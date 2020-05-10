07405291b9ba4dc49805289c0bf7f2c5ba25cd2d
Wojciech Pajak <wojciechpaj@gmail.com>
2020-05-10 11:49:34 +0200

    Create logic for win/lose

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,3 +1,28 @@
 from random import choice
 options = ("ROCK","PAPER","SCISSORS")
-print(choice(options))
+
+player = input("Please insert your choice: ROCK, PAPER or SCISSORS:\n").upper()
+computer = choice(options)
+print (f"Computer plays {computer}")
+
+if player == "ROCK":
+    if computer == "SCISSORS":
+        print("Player wins!")
+    elif computer == "PAPER":
+        print("Computer wins!")
+    else:
+        print("It's a tie!")
+elif player == "PAPER":
+    if computer == "ROCK":
+        print("Player wins!")
+    elif computer == "SCISSORS":
+        print("Computer wins!")
+    else:
+        print("It's a tie!")
+elif player == "SCISSORS":
+    if computer == "PAPER":
+        print("Player wins!")
+    elif computer == "ROCK":
+        print("Computer wins!")
+    else:
+        print("It's a tie!")
