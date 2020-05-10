dce3a416ee0cbb6318da46aa17744fb2930e4af2
Wojciech Pajak <wojciechpaj@gmail.com>
2020-05-10 12:02:09 +0200

    Added user input control checks

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,28 +1,36 @@
 from random import choice
-options = ("ROCK","PAPER","SCISSORS")
+options_comp = ("ROCK","PAPER","SCISSORS")
+options_player = ("ROCK","PAPER","SCISSORS","QUIT")
+while True:
+    player = input("\nPlease insert your choice: ROCK, PAPER or SCISSORS. Input QUIT to terminate.\n\n").upper()
+    while player not in options_player:
+        player = input("\nPlease insert a valid choice: ROCK, PAPER or SCISSORS. Input QUIT to terminate.\n\n").upper()
+    if player =="QUIT":
+        break
 
-player = input("Please insert your choice: ROCK, PAPER or SCISSORS:\n").upper()
-computer = choice(options)
-print (f"Computer plays {computer}")
+    computer = choice(options_comp)
+    print (f"\nComputer plays {computer}\n")
 
-if player == "ROCK":
-    if computer == "SCISSORS":
-        print("Player wins!")
-    elif computer == "PAPER":
-        print("Computer wins!")
-    else:
-        print("It's a tie!")
-elif player == "PAPER":
-    if computer == "ROCK":
-        print("Player wins!")
-    elif computer == "SCISSORS":
-        print("Computer wins!")
-    else:
-        print("It's a tie!")
-elif player == "SCISSORS":
-    if computer == "PAPER":
-        print("Player wins!")
-    elif computer == "ROCK":
-        print("Computer wins!")
-    else:
-        print("It's a tie!")
+    if player == "ROCK":
+        if computer == "SCISSORS":
+            print("Player wins!")
+        elif computer == "PAPER":
+            print("Computer wins!")
+        else:
+            print("It's a tie!")
+    elif player == "PAPER":
+        if computer == "ROCK":
+            print("Player wins!")
+        elif computer == "SCISSORS":
+            print("Computer wins!")
+        else:
+            print("It's a tie!")
+    elif player == "SCISSORS":
+        if computer == "PAPER":
+            print("Player wins!")
+        elif computer == "ROCK":
+            print("Computer wins!")
+        else:
+            print("It's a tie!")
+
+print("\nThanks for playing!\n")
